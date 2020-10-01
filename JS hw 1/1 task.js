337100a2ebb0f29ca00ee56b884c0948de945297
andreiha <z.karolin@gmail.com>
2020-10-01 19:51:51 +0300

    feat: add first task

--- JS hw 1/1 task.js
+++ JS hw 1/1 task.js
@@ -1,4 +1,13 @@
-function target (nums) {
-    nums.sort();
-        for (i = 0; i < nums.length; i++) {
-            if (nums[i] === nums[i+1]) {
+function target (nums, targ) {
+    for (i = 0; i < nums.length; i++) {
+        for (j = 0; j < nums.length; j++) {     
+            if (i != j && nums[i] + nums [j] === targ) {
+                return `${nums[i]}, ${nums[j]}`;
+            };
+        };
+    };
+};
+
+target (nums = [4, 2, 7, 11, 15], targ = 9);
+target (nums = [26, 3, 2, 4, 7, 1], target = 6);
+target (nums = [3, 3, 4, 5, 9], target = 6);
